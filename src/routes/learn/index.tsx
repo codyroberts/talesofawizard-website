c97aa58c1d6c85cbc1f6c7b1c8f83ba0338e56ff
codyroberts <codyaaronroberts@gmail.com>
2024-05-07 23:35:20 -0500

    Basic coming soon pages added.

--- src/routes/learn/index.tsx
+++ src/routes/learn/index.tsx
@@ -1,10 +1,14 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import ImgArchivist from "../../media/Archivist.png?jsx";
 
 export default component$(() => {
   return (
-    <>
-    </>
+    <div class="flex flex-col h-full gap-10 items-center justify-center mt-20 p-0">
+      <p class="text-6xl text-center">The archives are not ready for guests.</p> 
+      <ImgArchivist class="w-60 h-auto" />
+      <p class="text-5xl text-center">Check back soon!</p>
+    </div>
   );
 });
 
--- src/routes/play/index.tsx
+++ src/routes/play/index.tsx
@@ -1,10 +1,14 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import ImgGoblin from "../../media/goblin.png?jsx";
 
 export default component$(() => {
   return (
-    <>
-    </>
+    <div class="flex flex-col h-full gap-10 items-center justify-center mt-20 p-0">
+      <p class="text-6xl text-center">The goblins are still setting up.</p> 
+      <ImgGoblin class="w-60 h-auto" />
+      <p class="text-5xl text-center">Check back soon!</p>
+    </div>
   );
 });
 
