fc2f6e8b3a6b74696914470996af8b373da24f80
codyroberts <codyaaronroberts@gmail.com>
2024-05-10 23:22:54 -0500

    Began implementing collapsible mobile navigation. Invisible tailwindcss class not working properly.

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -4,34 +4,40 @@ import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <div class="flex items-center justify-evenly">
-    <ul class="flex items-center list-none gap-8 m-0 p-0">
+    <div class="flex lg:items-center lg:justify-evenly">
+    <ul class="flex items-center flex-col lg:flex-row list-none lg:gap-8 m-0 p-0">
 
-        <li class="inline-block m-0 p-0 text-xs">
-        <Link class="inline-block text-dark-text text-sm no-underline p-0 hover:text-highlight md:text-2xl" href="/play">
+        <li id="navElement" class="inline-block m-0 p-0 text-xs">
+        <Link class="max-sm:invisible lg:inline-block text-dark-text text-sm no-underline p-0 hover:text-highlight md:text-2xl" href="/play">
             Play
         </Link>
         </li>
-        <li class="inline-block m-0 p-0 text-xs">
-        <Link class="inline-block text-dark-text text-sm no-underline p-0 hover:text-highlight md:text-2xl" href="/learn">
+        <li id="navElement" class="inline-block m-0 p-0 text-xs">
+        <Link class="max-sm:invisible lg:inline-block text-dark-text text-sm no-underline p-0 hover:text-highlight md:text-2xl" href="/learn">
             Learn
         </Link>
         </li>
-        <li class="inline-block m-0 p-0 text-xs"> 
-        <Link class="inline-block text-dark-text text-sm no-underline p-0 hover:text-highlight md:text-2xl" href="/">
+        <li class="max-sm:order-first inline-block m-0 p-0 text-xs"> 
+        <Link onClick$={() => {
+
+            document.querySelectorAll('[id=navElement]').forEach((element)=>{
+                element.classList.toggle("max-sm:invisible");
+            });
+        
+        }} class="inline-block text-dark-text text-sm no-underline p-0 hover:text-highlight md:text-2xl">
             <ImgBook class="max-w-28 h-auto m-0 p-0 hover:drop-shadow-highlight" />
         </Link>
         </li>
-        <li class="inline-block m-0 p-0 text-xs">
-        <Link class="inline-block text-dark-text text-sm no-underline p-0 hover:text-highlight md:text-2xl"
+        <li id="navElement" class="inline-block m-0 p-0 text-xs">
+        <Link class="max-sm:invisible lg:inline-block text-dark-text text-sm no-underline p-0 hover:text-highlight md:text-2xl"
             href="https://github.com/codyroberts/talesofawizard"
             target="_blank"
         >
             Github
         </Link>
         </li>
-        <li class="inline-block m-0 p-0 text-xs">
-        <Link class="inline-block text-dark-text text-sm no-underline p-0 hover:text-highlight md:text-2xl"
+        <li id="navElement" class="inline-block m-0 p-0 text-xs">
+        <Link class="max-sm:invisible lg:inline-block text-dark-text text-sm no-underline p-0 hover:text-highlight md:text-2xl"
             href="https://discord.gg/rjX2JbYAVR"
             target="_blank"
         >
