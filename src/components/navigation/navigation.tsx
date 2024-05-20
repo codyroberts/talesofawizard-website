a7dafcf5ccfbd8d37862ed7992c9034b261746d1
codyroberts <codyaaronroberts@gmail.com>
2024-05-19 22:22:55 -0500

    Added dropdown menus to desktop navigation

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -25,8 +25,31 @@ export default component$(() => {
       {/* Desktop */}
       <div class="hidden lg:flex lg:items-center lg:justify-evenly">
         <ul class="m-0 flex list-none flex-col items-center p-0 lg:flex-row lg:gap-8">
-          <HeaderLink href="/play">Play</HeaderLink>
-          <HeaderLink href="/learn">Learn</HeaderLink>
+          <li class="group relative">
+            <p class="cursor-pointer md:text-2xl">Game</p>
+            <ul class="absolute left-0 top-full hidden list-none hover:block group-hover:block">
+              <li>
+                <HeaderLink href="/play">Play</HeaderLink>
+              </li>
+            </ul>
+          </li>
+          <li class="group relative">
+            <p class="cursor-pointer md:text-2xl">Learn</p>
+            <ul class="absolute left-0 top-full hidden list-none hover:block group-hover:block">
+              <li>
+                <HeaderLink href="/learn">Story</HeaderLink>
+              </li>
+              <li>
+                <HeaderLink href="/learn">Classes</HeaderLink>
+              </li>
+              <li>
+                <HeaderLink href="/learn">Races</HeaderLink>
+              </li>
+              <li>
+                <HeaderLink href="/learn">World</HeaderLink>
+              </li>
+            </ul>
+          </li>
           <li class="m-0 inline-block p-0 text-xs">
             <a
               class="inline-block p-0 text-sm text-dark-text no-underline hover:text-highlight md:text-2xl"
@@ -35,12 +58,37 @@ export default component$(() => {
               <ImgBook class="m-0 h-auto max-w-24 p-0 hover:drop-shadow-highlight lg:max-w-28" />
             </a>
           </li>
-          <HeaderLink href="https://github.com/codyroberts/talesofawizard">
-            Github
-          </HeaderLink>
-          <HeaderLink href="https://discord.gg/rjX2JbYAVR">Discord</HeaderLink>
+          <li class="group relative">
+            <p class="cursor-pointer md:text-2xl">Github</p>
+            <ul class="absolute left-0 top-full hidden list-none hover:block group-hover:block">
+              <li>
+                <HeaderLink href="https://github.com/codyroberts/talesofawizard-website">
+                  Website
+                </HeaderLink>
+              </li>
+              <li>
+                <HeaderLink href="https://github.com/codyroberts/talesofawizard">
+                  Game
+                </HeaderLink>
+              </li>
+            </ul>
+          </li>
+          <li class="group relative">
+            <p class="cursor-pointer md:text-2xl">Community</p>
+            <ul class="absolute left-0 top-full hidden list-none hover:block group-hover:block">
+              <li>
+                <HeaderLink href="https://discord.gg/rjX2JbYAVR">
+                  Discord
+                </HeaderLink>
+              </li>
+              <li>
+                <HeaderLink href="/">Leaderboards</HeaderLink>
+              </li>
+            </ul>
+          </li>
         </ul>
       </div>
+
       {/* Mobile */}
       <div class="mt-96 flex justify-evenly lg:hidden">
         <div class="flex-col justify-between">
