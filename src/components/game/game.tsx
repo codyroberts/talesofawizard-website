4fa1b7c3971222d21fc63d6cd0425641728251f5
codyroberts <codyaaronroberts@gmail.com>
2024-05-27 12:19:22 -0500

    Added some Pixi configurations

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -1,18 +1,15 @@
 import { component$, useVisibleTask$ } from "@builder.io/qwik";
-import { Application } from "pixi.js";
+import { Application, Assets } from "pixi.js";
 
 export default component$(() => {
   useVisibleTask$(async () => {
-    // Create a PixiJS application.
     const app = new Application();
 
-    // Intialize the application.
-    await app.init({ background: "#E6EEF7" });
-
     const gameDIV = document.getElementById("game");
-    // Then adding the application's canvas to the DOM body.
+
+    await app.init({ background: "#E6EEF7", resizeTo: gameDIV! });
     gameDIV.appendChild(app.canvas);
   });
 
-  return <div id="game" />;
+  return <div id="game" class="w-game h-game" />;
 });
