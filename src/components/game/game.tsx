a4a76e83eec17307b07dcb92fb171ce6c68d291c
codyroberts <codyaaronroberts@gmail.com>
2024-05-28 11:01:06 -0500

    Successfully loading assets. May need to move from assets folder to public folder to retain file names for Pixijs.

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -1,14 +1,19 @@
 import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import { Application, Assets } from "pixi.js";
+import OrcSprite from "../../assets/Characters/Orc/walk.png?url";
+import OrcSpriteData from "../../assets/Characters/Orc/walk.json?url";
 
 export default component$(() => {
+
   useVisibleTask$(async () => {
     const app = new Application();
+
+    Assets.load([OrcSprite, OrcSpriteData]);
 
     const gameDIV = document.getElementById("game");
 
     await app.init({ background: "#E6EEF7", resizeTo: gameDIV! });
-    gameDIV.appendChild(app.canvas);
+    gameDIV!.appendChild(app.canvas);
   });
 
   return <div id="game" class="w-game h-game" />;
