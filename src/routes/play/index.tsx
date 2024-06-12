b7a43dec8fe28f790e30b64f719289a4cf0c34e2
codyroberts <codyaaronroberts@gmail.com>
2024-06-11 21:18:24 -0500

    Styled parent div for play route

--- src/routes/play/index.tsx
+++ src/routes/play/index.tsx
@@ -3,7 +3,11 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 import Game from "~/components/game/game";
 
 export default component$(() => {
-  return <Game />;
+  return (
+    <div class="mt-20 flex h-full flex-col items-center justify-center gap-10 p-0">
+      <Game />;
+    </div>
+  );
 });
 
 export const head: DocumentHead = {
