ab7edc4de1afb96b4c3bf89c39a2c327bbf9a0ea
codyroberts <codyaaronroberts@gmail.com>
2024-06-20 07:33:38 -0500

    Fixed asset import path for 404 image

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -1,6 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import ImgGoblin from "../../media/goblin.png?jsx";
+import ImgGoblin from "../media/goblin.png?jsx";
 
 export default component$(() => {
   return (
