a4beafc2392c85dd178988146fa5daa361fb3900
codyroberts <codyaaronroberts@gmail.com>
2025-01-26 22:24:48 -0600

    refactored navigation

---
+++ src/app/components/navigation/components/desktop/desktop.tsx
@@ -0,0 +1,57 @@
+'use client';
+import React from 'react';
+import Menu from './components/menu/menu';
+import Item from './components/item/item';
+import styles from './desktop.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface MenuItem {
+  href: string;
+  text: string;
+}
+
+interface Menu {
+  header: string;
+  items: MenuItem[];
+}
+
+interface Props {
+  menus: Menu[];
+}
+
+const DesktopNavigation: React.FC<Props> = ({ menus }) => {
+  console.log(menus);
+
+  const handleBookClick = () => {
+    window.location.href = '/';
+  };
+
+  return (
+    <div className={styles.container}>
+      {menus.map((menu: Menu, menuIndex: number) => {
+        return (
+          <div key={menuIndex}>
+            <Menu header={menu.header}>
+              {menu.items.map((item: Item, itemIndex: number) => (
+                <Item key={itemIndex} href={item.href} text={item.text} />
+              ))}
+            </Menu>
+          </div>
+        );
+      })}
+      <Link href="/">
+        <Image
+          src="/images/OldBook.png"
+          width={2100}
+          height={2100}
+          alt="Home Button"
+          className={styles.image}
+          onClick={handleBookClick}
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default DesktopNavigation;
--- src/app/components/navigation/navigation.tsx
+++ src/app/components/navigation/navigation.tsx
@@ -1,11 +1,8 @@
 'use client';
-import React, { useState } from 'react';
-import Menu from './components/menu/menu';
-import Item from './components/item/item';
+import React from 'react';
 import styles from './navigation.module.scss';
-import Image from 'next/image';
-import Link from 'next/link';
-import getDeviceType from '@/app/utility/deviceDetection';
+import DesktopNavigation from './components/desktop/desktop';
+import MobileNavigation from './components/mobile/mobile';
 
 const items = {
   game: [{ href: '/game', text: 'Play' }],
@@ -36,42 +33,14 @@ const menus = [
 ];
 
 const Navigation: React.FC = () => {
-  const [open, setOpen] = useState(false);
-  const device: string | undefined = getDeviceType();
-
-  const handleBookClick = () => {
-    if (device == 'Desktop' || device === 'Tablet') {
-      window.location.href = '/';
-    } else {
-      setOpen(!open);
-    }
-  };
-
   return (
     <div className={styles.container}>
-      <div className={`${styles.menus} ${open ? styles.show : ''}`}>
-        {menus.map((menu: Menu, menuIndex: number) => {
-          return (
-            <div key={menuIndex}>
-              <Menu header={menu.header}>
-                {menu.items.map((item: Item, itemIndex: number) => (
-                  <Item key={itemIndex} href={item.href} text={item.text} />
-                ))}
-              </Menu>
-            </div>
-          );
-        })}
+      <div className={styles.desktop}>
+        <DesktopNavigation menus={menus} />
       </div>
-      <Link href="/">
-        <Image
-          src="/images/OldBook.png"
-          width={2100}
-          height={2100}
-          alt="Home Button"
-          className={styles.image}
-          onClick={handleBookClick}
-        />
-      </Link>
+      <div className={styles.mobile}>
+        <MobileNavigation menus={menus} />
+      </div>
     </div>
   );
 };
