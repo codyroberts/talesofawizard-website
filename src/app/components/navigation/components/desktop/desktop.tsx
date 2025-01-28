7024092b4fef13fad0b7a7fe874e669d1458ce22
codyroberts <codyaaronroberts@gmail.com>
2025-01-27 23:20:26 -0600

    fixed types

--- src/app/components/navigation/components/desktop/desktop.tsx
+++ src/app/components/navigation/components/desktop/desktop.tsx
@@ -6,19 +6,19 @@ import styles from './desktop.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
 
-interface MenuItem {
+type MenuItem = {
   href: string;
   text: string;
-}
+};
 
-interface Menu {
+type Menu = {
   header: string;
   items: MenuItem[];
-}
+};
 
-interface Props {
+type Props = {
   menus: Menu[];
-}
+};
 
 const DesktopNavigation: React.FC<Props> = ({ menus }) => {
   console.log(menus);
@@ -33,7 +33,7 @@ const DesktopNavigation: React.FC<Props> = ({ menus }) => {
         return (
           <div key={menuIndex}>
             <Menu header={menu.header}>
-              {menu.items.map((item: Item, itemIndex: number) => (
+              {menu.items.map((item: MenuItem, itemIndex: number) => (
                 <Item key={itemIndex} href={item.href} text={item.text} />
               ))}
             </Menu>
